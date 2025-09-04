3fa34beacccf0f181d5ea45debdd931218aca669
Bharath-Krishna-AB <bharatha.b2007@gmail.com>
2025-09-04 08:47:40 +0530

    completed

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 var express = require('express');
 var cors = require('cors');
+const bodyParser = require('body-parser');
+const multer = require('multer')
+const upload = multer({ dest: 'public/' });
 require('dotenv').config()
 
 var app = express();
-
+// app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
+
+app.post('/api/fileanalyse',upload.single('upfile') , (req, res) =>{
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
 
 
 
